e5d4ea7a6002b0c81c00194d163671125c51b0e2
cd4761 <cd4761@naver.com>
2019-07-24 16:48:29 +0900

    modify eccpow to get mixhash

--- eth/consensus/pow.py
+++ eth/consensus/pow.py
@@ -104,9 +104,10 @@ def check_eccpow(previous_header: Hash32,
     validate_length(current_header, 32, title="Current Hash")
     # mix_hash = b"0x0000000000000000000000000000000000000000000000000000000000000000"
     mix_hash = '0x000'
+    mix_hash = encode_hex(mix_hash)
     # mining_output = pyecceth.eth_ecc(previous_header, current_header, n, wc, wr)
     mining_output = pyeccpow.eth_ecc(previous_header, current_header, n, wc, wr)
-    if mining_output[b'mix digest'] != encode_hex(mix_hash):
+    if mining_output[b'mix digest'] != mix_hash:
         raise ValidationError("mix hash mismatch; {0} != {1}".format(
             encode_hex(mining_output[b'mix digest']), encode_hex(mix_hash)))
     result = big_endian_to_int(mining_output[b'result'])
